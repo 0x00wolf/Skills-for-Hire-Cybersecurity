3e5d80236610330d6ac3f414d12a9e0e58edfb3a
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-08-31 15:27:23 -0700

    Update unixclock.py

--- Log-File-Analysis/unixclock.py
+++ Log-File-Analysis/unixclock.py
@@ -2,21 +2,21 @@ import time
 from sys import exit
 
 def clock():
-    
-    while True:
-        try:
-            utc_offset = (-time.timezone)
-            unix_timestamp = time.time()
-            t = time.localtime()
-            time_ = time.strftime("%H:%M:%S", t)
-            print('\n'*40)
-            print(f'current time: {time_}')
-            print(f'unix timestamp: {unix_timestamp}')
-            time.sleep(0.1)
-        except KeyboardInterrupt:
-            exit(0)
+    try:
+        utc_offset = (-time.timezone)
+        unix_timestamp = time.time()
+        t = time.localtime()
+        time_ = time.strftime("%H:%M:%S", t)
+        print('\n'*40)
+        print(f'current time: {time_}')
+        print(f'unix timestamp: {unix_timestamp}')
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        exit(0)
+        
 
 if __name__ == '__main__':
-    clock()
+    while True:
+        clock()
 
 
